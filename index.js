b4438c59d1e97b30d14ae4454ef6a8eb68a58687
NikaPeretrukhina <nika.peretrukhina@gmail.com>
2026-02-06 18:41:16 +0100

    test for first team

---
+++ index.js
@@ -0,0 +1,23 @@
+let firstTeam = document.getElementById("team-1")
+let secondTeam = document.getElementById("team-2")
+let onePoint = document.getElementById("point-1")
+let twoPoint = document.getElementById("point-2")
+let threePoint = document.getElementById("point-3")
+
+let count = 0
+
+function incrementOne(){
+count +=1
+firstTeam.textContent = count
+
+}
+function incrementTwo(){
+count +=2
+
+
+}
+function incrementThree(){
+count +=3
+
+
+}
