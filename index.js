ecff262a5d5cfd2f84a6af28e1252c224199b71c
NikaPeretrukhina <nika.peretrukhina@gmail.com>
2026-02-09 00:00:00 +0100

    Implement scoring logic

--- index.js
+++ index.js
@@ -1,23 +1,38 @@
 let firstTeam = document.getElementById("team-1")
 let secondTeam = document.getElementById("team-2")
-let onePoint = document.getElementById("point-1")
-let twoPoint = document.getElementById("point-2")
-let threePoint = document.getElementById("point-3")
 
-let count = 0
 
-function incrementOne(){
- count +=1
- firstTeam.textContent = count
+let count1 = 0
+let count2 = 0
 
+
+//schreib nur eine functionn wo du parameter nutzt um mit daten zu arbeiten
+function increment(team, points) {
+   if(team === firstTeam){
+    count1 =count1 + points
+    console.log(count1)
+    firstTeam.textContent = count1 
+    
+   }else{
+    count2 +=points
+    secondTeam.textContent=count2
+    console.log(count2)
+   }
+    
 }
-function incrementTwo(){
- count +=2
- firstTeam.textContent = count
 
-}
-function incrementThree(){
- count +=3
- firstTeam.textContent = count
 
-}
+// function incrementTwo(){
+//  count1 +=2
+//  firstTeam.textContent=count1
+//  count2 +=2
+//  secondTeam.textContent = count2
+
+// }
+// function incrementThree(){
+//  count1 +=3
+//  firstTeam.textContent=count1
+//  count2 +=3
+//  secondTeam.textContent= count2
+
+// }
